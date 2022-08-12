8fb2fbd1b6baae23e5f3f003edc6b250ba69b470
Andrius <adesseno.arq@gmail.com>
2022-08-12 20:57:34 -0300

    cambios en app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const arregloDeNumeros = [1,2,3,4,5,6,7,8,9]
 
 const strings = arregloDeNumeros.map((e) => e.toString())
-
+const total = arregloDeNumeros.reduce((acc, e) => acc + e, 0)
+const stringConcat = strings.reduce((acc, e) => acc + e, "")
 console.log(strings)
+console.log(total)
+console.log(stringConcat)
